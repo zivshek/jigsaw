8cd3cfabf0198453eb6f4ec81e8d0d07e182a37a
zivshek <ziv.hao.shek@gmail.com>
2022-09-11 23:36:22 -0400

    Added padding and spacing to flexible layout group

--- Assets/Scripts/Utils/FlexibleGridLayout.cs
+++ Assets/Scripts/Utils/FlexibleGridLayout.cs
@@ -1,21 +1,49 @@
 using UnityEngine;
 using UnityEngine.UI;
 
+public enum FitType
+{
+    Uniform, Width, Height, FixedRows, FixedColumns
+}
+
 public class FlexibleGridLayout : LayoutGroup
 {
+    public FitType FitType;
     public int Rows;
     public int Cols;
     public Vector2 CellSize;
     public Vector2 Spacing;
 
+    [SerializeField][ReadOnly]
+    private bool fitX;
+    [SerializeField][ReadOnly]
+    private bool fitY;
+
     public override void CalculateLayoutInputHorizontal()
     {
         base.CalculateLayoutInputHorizontal();
 
-        Rows = Cols = Mathf.CeilToInt(Mathf.Sqrt(transform.childCount));
+        if (FitType == FitType.Uniform || FitType == FitType.Width || FitType == FitType.Height)
+        {
+            fitX = true;
+            fitY = true;
+            Rows = Cols = Mathf.CeilToInt(Mathf.Sqrt(transform.childCount));
+        }
 
-        CellSize = new Vector2(rectTransform.rect.width / Cols - ((Spacing.x / Cols) * (Cols - 1)),
-            rectTransform.rect.height / Rows - ((Spacing.y / Rows) * (Rows - 1)));
+        if (FitType == FitType.Width || FitType == FitType.FixedColumns)
+        {
+            Rows = Mathf.CeilToInt(transform.childCount / Cols);
+        }
+        else if (FitType == FitType.Height || FitType == FitType.FixedRows)
+        {
+            Cols = Mathf.CeilToInt(transform.childCount / Rows);
+        }
+
+        float cellWidth = rectTransform.rect.width / Cols - ((Spacing.x / Cols) * (Cols - 1)) - padding.left / Cols - padding.right / Cols;
+        float cellHeight = rectTransform.rect.height / Rows - ((Spacing.y / Rows) * (Rows - 1)) - padding.top / Rows - padding.bottom / Rows;
+        
+        CellSize.x = fitX ? cellWidth : CellSize.x;
+        CellSize.y = fitY ? cellHeight : CellSize.y;
 
         for (int i = 0; i < rectChildren.Count; ++i)
         {
@@ -23,8 +51,8 @@ public class FlexibleGridLayout : LayoutGroup
             int colIndex = i % Cols;
             var element = rectChildren[i];
 
-            var xPos = (CellSize.x * colIndex) + (Spacing.x * colIndex);
-            var yPos = (CellSize.y * rowIndex) + (Spacing.y * rowIndex);
+            var xPos = (CellSize.x * colIndex) + (Spacing.x * colIndex) + padding.left;
+            var yPos = (CellSize.y * rowIndex) + (Spacing.y * rowIndex) + padding.top;
 
             SetChildAlongAxis(element, 0, xPos, CellSize.x);
             SetChildAlongAxis(element, 1, yPos, CellSize.y);
